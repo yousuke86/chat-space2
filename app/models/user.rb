dd2a7f0f88817c5ecd66eddee54727565a8133aa
yousuke86 <yousuke7201@yahoo.co.jp>
2020-06-06 15:59:27 +0900

    group-imp

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  # has_many :messages
+  has_many :groups, through: :group_users
+  has_many :group_users
+
   validates :name, presence: true, uniqueness: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root to: "messages#index"
+  root to: "groups#index"
+
   resources :users, only: [:edit, :update,]
+  resources :groups, only: [:index, :new, :create, :edit, :update]
 end
